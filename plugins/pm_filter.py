8784f5b13378cfa5f97fe77215f404887d19e986
varshneyom2010 <varshneyom2010@gmail.com>
2026-07-01 08:49:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -30,11 +30,15 @@ BUTTONS = {}
 SPELL_CHECK = {}
 
 
-@Client.on_message((filters.group | filters.private) & filters.text & filters.incoming)
+@Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
         await auto_filter(client, message)
+
+@Client.on_message(filters.private & filters.text & filters.incoming)
+async def give_pm_filter(client, message):
+    await pm_auto_filter(client, message)
 
 
 @Client.on_callback_query(filters.regex(r"^next"))
